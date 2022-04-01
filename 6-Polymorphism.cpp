11bad58499ff05f5c2bc443c0164bd76925339ef
Gopi <gopirudra29@gmail.com>
2022-04-01 08:18:45 +0530

    Changes

--- 6-Polymorphism.cpp
+++ 6-Polymorphism.cpp
@@ -88,3 +88,67 @@ int main()
 
     p->start(); // Output - AdvanceCar Started
 }
+
+/*
+    *Abstract Function
+    If a member function is not having a body then it is an abstract function.
+
+Example:
+class Test
+{
+    public:
+        void fun(); // body is not defined outside
+};
+
+*Abstract Class
+    If a class is having an abstract function then class becomes abstract.
+    If a class is having pure virtual function then it becomes abstract.
+    We can declare pointer of abstract class
+    We can create object of abstract class but cannot call undefined functions.
+    Abstract classes are useful for achieving polymorphism.
+
+*Can we overriding private function?
+    you can override but can't achieve polymorphism because private members cannot be called on object.
+
+*If a base class has both concrete functions and pure virtual functions then can we create an object of that base class?
+Yes you can create the object of a class but cannot call undefined functions.
+
+*/
+
+class car
+{
+public:
+    virtual void start()
+    {
+        cout << "car started" << endl;
+    }
+    /*
+        virtual void start()=0;
+        "PURE Virtual Function"
+    */
+};
+
+class innova : public car
+{
+public:
+    void start()
+    {
+        cout << "innova started" << endl;
+    }
+};
+
+class swift : public car
+{
+public:
+    void start()
+    {
+        cout << "swift started" << endl;
+    }
+};
+int main()
+{
+    car *ptr = new innova();
+    ptr->start();
+    p = new swift();
+    ptr->start();
+}
